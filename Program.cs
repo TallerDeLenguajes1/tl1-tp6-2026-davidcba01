a9733bd79b9e5d083176fd4fd268a0b1f73b2a25
davidcba01 <davoocordoba2001@gmail.com>
2026-06-02 01:14:50 -0300

    Resolucion de Ejercicio 1

--- Program.cs
+++ Program.cs
@@ -2,7 +2,49 @@
 Console.WriteLine("Hello, World!");
 int a;
 int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+// Ejercicio 1. Construir un programa que permita invertir un número. Verifique que el 
+// texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del 
+// número sólo si éste es mayor a 0.
+// Nota: Si observa un subrayado amarillo sugiriendo modificar las variables al tipo 
+// "nullable", abra el archivo de configuración de proyecto de extensión .csproj y elimine 
+// la línea que dice <Nullable>enable</Nullable>. No olvide guardar el archivo.
+
+int numero = 0;
+
+Console.Write("Ingrese un numero que quiera invertir (entero positivo): ");
+
+string entrada = Console.ReadLine();
+
+bool esNumero = int.TryParse(entrada, out numero);
+
+if (esNumero)
+{
+    Console.WriteLine($"El número ingresado es: {numero}");
+
+    if (numero > 0)
+    {
+        int numeroInvertido = 0;
+
+        while (numero > 0)
+        {
+            int digito = numero % 10;
+            numeroInvertido = (numeroInvertido * 10) + digito;
+            numero /= 10;
+        }
+
+        Console.WriteLine("El numero invertido es: " + numeroInvertido);
+    }
+    else
+    {
+        Console.WriteLine("El número debe ser mayor que 0");
+    }
+}
+else
+{
+    Console.WriteLine("La entrada no es un número entero válido");
+}
